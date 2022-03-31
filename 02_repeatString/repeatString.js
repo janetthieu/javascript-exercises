0320bfbeef1236cf83a03f46303f4697a7d4ce27
Janet Thieu <janet.thieu@gmail.com>
2022-03-30 21:41:37 -0400

    fix: create function to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) 
+        return 'ERROR';
 
+    else if (num === 0)
+        return '';
+    
+    else if (num === 1)
+        return string;
+
+    else
+        return string + repeatString(string, num - 1);
 };
 
 // Do not edit below this line
