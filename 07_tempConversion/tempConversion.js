dcf691bc32856c0d4c5a4d87007d5843bb4d6f32
Janet Thieu <janet.thieu@gmail.com>
2022-04-02 18:07:59 -0400

    Fix: create functions to pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitTemp) {
+  celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
+  return Math.round(celsiusTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusTemp) {
+  fahrenheitTemp = celsiusTemp * (9 / 5) + 32;
+  return Math.round(fahrenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
