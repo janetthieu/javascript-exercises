d49299dccc4bdc9d7f254fab5e3514898e9e8641
Janet Thieu <janet.thieu@gmail.com>
2022-04-02 18:56:47 -0400

    Fix: create functions to pass tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const initialValue = 0;
+  const sumWithInitial = arr.reduce((previousValue, currentValue) =>
+    previousValue + currentValue, initialValue);
+
+  return sumWithInitial;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let newValue = arr[0];
 
+  for (let i = 1; i < arr.length; i++) {
+    newValue *= arr[i];
+  }
+
+  return newValue;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0)
+    return -1;
+
+  else if (num === 0)
+    return 1;
+    
+  else
+    return (num * factorial(num - 1));
 };
 
 // Do not edit below this line
