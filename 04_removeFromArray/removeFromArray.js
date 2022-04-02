60abf5187796cdb1477f028d066713ef85b7f0e9
Janet Thieu <janet.thieu@gmail.com>
2022-04-01 21:29:46 -0400

    Fix: create function to pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let args = Array.from(arguments);
+    
+    arr = arr.filter(function(value) {
+        if (args.indexOf(value) === -1)
+            return true;
+    })
 
+    return arr;
 };
 
 // Do not edit below this line
