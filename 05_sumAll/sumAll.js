1100dfdb40cc6fdeee07ed95dd9a61099a06de27
Janet Thieu <janet.thieu@gmail.com>
2022-04-01 23:17:00 -0400

    Fix: create function to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let values = x < y ? x : y;
+    let highestValue = x < y ? y : x;
+    let sum = 0;
 
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y))
+        return 'ERROR';
+
+    for (let i = 0; i < highestValue; i++) {
+        sum += values;
+        values++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
