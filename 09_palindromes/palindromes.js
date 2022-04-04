d49f48b14c66097fa4a3ceeb94bbd837b857777a
Janet Thieu <janet.thieu@gmail.com>
2022-04-03 23:19:37 -0400

    Fix: create function to pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    str = str.toLowerCase();
+    let regex = /\w/;
+    let reverseStr = '';
+    let compareStr = '';
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i].match(regex)) {
+            reverseStr += str[i];
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(regex)) {
+            compareStr += str[i];
+        }
+    }
+
+    if (compareStr === reverseStr)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
