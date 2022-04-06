06f773b954c0914827abaf26a2c863a2d1ad1ebd
Janet Thieu <janet.thieu@gmail.com>
2022-04-05 22:19:41 -0400

    Fix: create function to pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return 'OOPS';
+    } else if (n <= 2) {
+        n = 1;
+    } else {
+        n = fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    
+    return n;
 };
 
 // Do not edit below this line
