976eabd21484da2641121eb2b473d60e2015b088
Janet Thieu <janet.thieu@gmail.com>
2022-04-09 16:21:17 -0400

    Fix: create function to pass tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const today = new Date();
+    const year = today.getFullYear();
 
+    let agesOfPeople = people.map(function (person) {
+        if (!person.hasOwnProperty('yearOfDeath')) {
+            person.age = year - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    })
+
+    let oldestPerson = people.reduce((p1, p2) => p1.age > p2.age ? p1 : p2);
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
